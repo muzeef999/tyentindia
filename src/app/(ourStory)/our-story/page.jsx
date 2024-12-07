6f04bfcdeb20a36418a9c904b089a8dcf6631dd9
jkmultiMediaInstition <jkmultimediainstitution@gmail.com>
2024-12-07 11:17:46 +0530

    six commit

--- src/app/(ourStory)/our-story/page.jsx
+++ src/app/(ourStory)/our-story/page.jsx
@@ -4,7 +4,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { MotionPathPlugin } from "gsap/all";
 import gsap from "gsap";
 import Vision from "@/app/compoents/(story)/Vision";
- const Mission = dynamic(() => import('@//app/compoents/(story)/Mission'));
+ const Mission = dynamic(() => import('@//app/compoents/(story)/Mission'), { ssr: false });
 import Management from "@/app/compoents/(story)/Management";
 import Form from "@/app/compoents/Form/Form";
 import dynamic from "next/dynamic";

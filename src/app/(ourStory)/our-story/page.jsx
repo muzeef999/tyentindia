ca177c72eabde67bd5a9598858cce20de66e9bb4
jkmultiMediaInstition <jkmultimediainstitution@gmail.com>
2024-12-07 11:23:42 +0530

    six commit

--- src/app/(ourStory)/our-story/page.jsx
+++ src/app/(ourStory)/our-story/page.jsx
@@ -4,10 +4,10 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { MotionPathPlugin } from "gsap/all";
 import gsap from "gsap";
 import Vision from "@/app/compoents/(story)/Vision";
- const Mission = dynamic(() => import('@//app/compoents/(story)/Mission'), { ssr: false });
+//  const Mission = dynamic(() => import('@//app/compoents/(story)/Mission'), { ssr: false });
 import Management from "@/app/compoents/(story)/Management";
 import Form from "@/app/compoents/Form/Form";
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 
 gsap.registerPlugin(MotionPathPlugin, ScrollTrigger);
 
@@ -21,7 +21,7 @@ const page = () => {
       <Vision />
       <br />
       <br />
-      <Mission />
+      {/* <Mission /> */}
       <br />
       <br/>
       <Management />

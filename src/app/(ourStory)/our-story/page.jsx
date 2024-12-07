fca9825d5098a53c9b7b643cc6eb7ab985315408
jkmultiMediaInstition <jkmultimediainstitution@gmail.com>
2024-12-07 11:25:29 +0530

    seven commit

--- src/app/(ourStory)/our-story/page.jsx
+++ src/app/(ourStory)/our-story/page.jsx
@@ -1,35 +1,7 @@
-"use client"
-import React from "react";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { MotionPathPlugin } from "gsap/all";
-import gsap from "gsap";
-import Vision from "@/app/compoents/(story)/Vision";
-//  const Mission = dynamic(() => import('@//app/compoents/(story)/Mission'), { ssr: false });
-import Management from "@/app/compoents/(story)/Management";
-import Form from "@/app/compoents/Form/Form";
-// import dynamic from "next/dynamic";
+import React from 'react'
 
-gsap.registerPlugin(MotionPathPlugin, ScrollTrigger);
-
-const page = () => {
- 
-
+export const page = () => {
   return (
-    <div>
-      <br />
-      <br />
-      <Vision />
-      <br />
-      <br />
-      {/* <Mission /> */}
-      <br />
-      <br/>
-      <Management />
-      <br />
-      <br />
-      <Form />
-    </div>
-  );
-};
-
-export default page;
+    <div>page</div>
+  )
+}
